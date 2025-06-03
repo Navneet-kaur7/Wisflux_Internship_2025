3d422069b79bd44298c285ca28f0c4a3a621f0c8
Navneet-kaur7 <navneetgill1234@gmail.com>
2025-06-03 16:51:47 +0530

    operators

---
+++ JavaScript/Day02/Operators.js
@@ -0,0 +1,32 @@
+let x = 5;
+
+// addition operator
+console.log("Addition: x + 3 = ", x + 3);
+
+// subtraction operator
+console.log("Subtraction: x - 3 =", x - 3);
+
+// multiplication operator
+console.log("Multiplication: x * 3 =", x * 3);
+
+// division operator
+console.log("Division: x / 3 =", x / 3);
+
+// remainder operator
+console.log("Remainder: x % 3 =", x % 3);
+
+// increment operator
+console.log("Increment: ++x =", ++x);
+
+// decrement operator
+console.log("Decrement: --x =", --x);
+
+// exponentiation operator
+console.log("Exponentiation: x ** 3 =", x ** 3);
+
+//comparison operators
+console.log("Equal: x == 5", x == 5);
+console.log("Not Equal: x != 5", x != 5);     
+
+//logical operators
+console.log("Logical AND: (x > 3) && (x < 10)", (x > 3) && (x < 10));
