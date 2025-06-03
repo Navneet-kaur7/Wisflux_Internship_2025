6158483f6709e8887d633983c23004a363aad1db
Navneet-kaur7 <navneetgill1234@gmail.com>
2025-06-03 17:16:15 +0530

    datatypes

---
+++ JavaScript/Day02/dataTypes.js
@@ -0,0 +1,30 @@
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
+
+
+console.log("length:", length);
+console.log("weight:", weight);
+console.log("color:", color);
+console.log("lastName:", lastName);
+console.log("x:", x);
+console.log("y:", y);
+console.log("person:", person);
+console.log("cars:", cars);
+console.log("date:", date.toString());
