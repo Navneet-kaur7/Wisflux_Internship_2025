9cba8fa6433f9fba79ccc704c8be976d09a1583f
Navneet-kaur7 <navneetgill1234@gmail.com>
2025-06-14 17:35:49 +0530

    Middleware and CRUD Api

---
+++ ExpressJs/middleware.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+// Middleware 1: Log request method and URL
+app.use((req, res, next) => {
+    console.log(`${req.method} request to ${req.url}`);
+    next();
+});
+
+// Middleware 2: Add a custom header
+app.use((req, res, next) => {
+    res.setHeader('X-Custom-Header', 'Middleware Chaining Example');
+    console.log('Custom header added');
+    next();
+});
+
+// Route handler
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
+
